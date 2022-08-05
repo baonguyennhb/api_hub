ee08e953d94a97fd2c6b8cd91df3d461872f5a5d
HUU BAO <bao.nguyen@gmail.com>
2022-08-05 07:49:49 +0700

    update

--- Controller/device.controller.js
+++ Controller/device.controller.js
@@ -20,13 +20,14 @@ module.exports.postAdd = async (req, res) => {
 
   try {
     let data = req.body
+    console.log(data)
     let sql = `INSERT INTO Metter (Serial, Model, Description, Interval) Values ( ${data.serial}, '${data.model}', '${data.description}', ${data.interval} )`
       //let sql = 'SELECT * FROM Metter'
     const devices = await query(sql)
     const dataSend = {
         code: 200,
         message: "OK",
-        data: devices
+        data: data
     }
     res.status(200).send(dataSend)
   } catch (error) {
