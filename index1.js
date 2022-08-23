91f0b5446a4b2aff7ef09877c61b615f2c87d48b
HUU BAO <bao.nguyenhuu1999@gmail.com>
2022-08-23 09:25:15 +0700

    update

--- index1.js
+++ index1.js
@@ -49,16 +49,16 @@ const DeleteTagConfigMessage = require("./EdgeSdk/DeleteTagMessage")
 
 //=================================================
 // Init
-async function Init(){
+async function Init() {
   let config = await query(`SELECT * FROM DataHub`)
   let options = {
     port: 1883,
     username: config[0].username, //     'Goy2waYPAGQP:n3Q78J2BBKeK',
     password: config[0].password, //    'CVemCimzm0duGLr6OnvJ',
     reconnectPeriod: 2000,
-    connectTimeout: 1000
+    connectTimeout: 5000
   };
-  
+
   groupId = config[0].group_id //'scada_qQ2N60h1DmL'
   mqttUrl = "mqtt://" + config[0].host + ":" + config[0].port
   mqttTopicConn = `iot-2/evt/waconn/fmt/${groupId}`
@@ -67,20 +67,19 @@ async function Init(){
   HbtInterval = config[0].heart_beat
 
   client = mqtt.connect(mqttUrl, options);
-  
+
   console.log(options, config[0])
 
   client.on("connect", ack => {
     try {
       console.log("MQTT Client Connected!")
       const _connectionMessage = ConnectionMessage(groupId)
-      //const dataConfig = updateTag()
-      client.publish(mqttTopicConn, JSON.stringify(_connectionMessage), { qos: 1, retain: true })
+      client.publish(mqttTopicConn, JSON.stringify(_connectionMessage), { qos: 1, retain: false })
       console.log("Connect success!")
       setInterval(sendHeartBeatMessage, HbtInterval * 1000)
-      // Send Data
-      //client.publish(mqttTopicCfg, JSON.stringify(dataConfig))
-      //console.log(" Config tag success!")
+      // Send Tag Config
+      sendTagConfigMessage()
+      console.log(" Config tag success!")
       console.log("Connected")
     } catch (error) {
       console.log(error)
@@ -90,12 +89,12 @@ async function Init(){
   client.on("reconnect", ack => {
     try {
       console.log("MQTT Reconnect Connected!")
-      
+
     } catch (error) {
       console.log(error)
     }
   })
-  client.on("error", async function(ack){
+  client.on("error", async function (ack) {
     try {
       console.log("MQTT Error!", ack.message)
 
@@ -107,7 +106,7 @@ async function Init(){
         reconnectPeriod: 2000,
         connectTimeout: 1000
       };
-      
+
       groupId = config[0].group_id //'scada_qQ2N60h1DmL'
       mqttUrl = "mqtt://" + config[0].host + ":" + config[0].port
       mqttTopicConn = `iot-2/evt/waconn/fmt/${groupId}`
@@ -125,7 +124,7 @@ async function Init(){
   client.on("close", ack => {
     try {
       console.log("MQTT close!", ack.message)
-      
+
     } catch (error) {
       console.log(error)
     }
@@ -133,7 +132,7 @@ async function Init(){
   client.on("disconnect", ack => {
     try {
       console.log("MQTT disconnect!", ack.message)
-      
+
     } catch (error) {
       console.log(error)
     }
@@ -141,14 +140,14 @@ async function Init(){
   client.on("offline", ack => {
     try {
       console.log("MQTT offline!", ack.message)
-      
+
     } catch (error) {
       console.log(error)
     }
   })
   //==================================================
 
-  
+
 }
 
 Init()
@@ -399,412 +398,6 @@ async function getTagInRawNeedupdate(api_source, start1) {
 
 //============================================================
 
-const updateTag = () => {
-  let d = {}
-  const listModel = [20698013, 20697912, 20697917, 20697923, 20697924, 20697927, 20697996, 20697875, 20697666, 20697578, 20697586, 20697594]
-  let Utg = {}
-  for (let i = 0; i < listModel.length; i++) {
-    Utg[`${listModel[i]}:MA_DIEMDO`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:MA_DIEMDO`,
-      "TID": 1,
-      "Dsc": "MA_DIEMDO",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:SO_CTO`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:SO_CTO`,
-      "TID": 1,
-      "Dsc": "SO_CTO",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:IMPORT_KWH`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:IMPORT_KWH`,
-      "TID": 1,
-      "Dsc": "IMPORT_KWH",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:EXPORT_KWH`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:EXPORT_KWH`,
-      "TID": 1,
-      "Dsc": "EXPORT_KWH",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:IMPORT_VAR`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:IMPORT_VAR`,
-      "TID": 1,
-      "Dsc": "IMPORT_VAR",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:EXPORT_VAR`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:EXPORT_VAR`,
-      "TID": 1,
-      "Dsc": "EXPORT_VAR",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Ia`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Ia`,
-      "TID": 1,
-      "Dsc": "Ia",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Ib`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Ib`,
-      "TID": 1,
-      "Dsc": "Ib",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Ic`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Ic`,
-      "TID": 1,
-      "Dsc": "Ic",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Ua`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Ua`,
-      "TID": 1,
-      "Dsc": "Ua",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Ub`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Ub`,
-      "TID": 1,
-      "Dsc": "Ub",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Uc`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Uc`,
-      "TID": 1,
-      "Dsc": "Uc",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:Cosphi`] = {
-      "Log": 1,
-      "SH": 1000,
-      "SL": 0,
-      "EU": "",
-      "DSF": "4.2",
-      "Alm": false,
-      "Name": `${listModel[i]}:Cosphi`,
-      "TID": 1,
-      "Dsc": "Cosphi",
-      "RO": 0,
-      "Ary": 1
-    }
-    Utg[`${listModel[i]}:NGAYGIO`] = {
-      "Name": `${listModel[i]}:NGAYGIO`,
-      "TID": 3,
-      "Dsc": "NGAYGIO",
-      "RO": 0,
-      "Ary": 1
-    }
-
-    //UpdateTagList.push(UpdateTag)
-  }
-  d[`${groupId}`] = {
-    "TID": 1,
-    "Dsc": "descrp",
-    "Hbt": 60,
-    "PID": 1,
-    "BID": 0,
-    "UTg": Utg
-  }
-  const dataConfig = {
-    "d": d,
-    "ts": Date.now()
-  }
-  return dataConfig
-}
-
-const deleteTag = (tag) => {
-  let d = {}
-  const listModel = [20698013, 20697912, 20697917, 20697923, 20697924, 20697927, 20697996, 20697875, 20697666, 20697578, 20697586, 20697594]
-  let DTg = {}
-  DTg[`${tag}`] = 1
-  d[`${groupId}`] = {
-    "TID": 1,
-    "Dsc": "descrp",
-    "Hbt": 60,
-    "PID": 1,
-    "BID": 0,
-    "DTg": DTg,
-    "Del": 1
-  }
-  const dataConfig = {
-    "d": d,
-    "ts": Date.now()
-  }
-  return dataConfig
-}
-
-const deleteDevice = (serial) => {
-  let d = {}
-  const listModel = [20698013, 20697912, 20697917, 20697923, 20697924, 20697927, 20697996, 20697875, 20697666, 20697578, 20697586, 20697594]
-  let DTg = {}
-  DTg[`${serial}:MA_DIEMDO`] = 1
-  DTg[`${serial}:SO_CTO`] = 1
-  DTg[`${serial}:IMPORT_KWH`] = 1
-  DTg[`${serial}:EXPORT_KWH`] = 1
-  DTg[`${serial}:IMPORT_VAR`] = 1
-  DTg[`${serial}:EXPORT_VAR`] = 1
-  DTg[`${serial}:Ia`] = 1
-  DTg[`${serial}:Ib`] = 1
-  DTg[`${serial}:Ic`] = 1
-  DTg[`${serial}:Ua`] = 1
-  DTg[`${serial}:Ub`] = 1
-  DTg[`${serial}:Uc`] = 1
-  DTg[`${serial}:Cosphi`] = 1
-  DTg[`${serial}:NGAYGIO`] = 1
-
-  d[`${groupId}`] = {
-    "TID": 1,
-    "Dsc": "descrp",
-    "Hbt": 60,
-    "PID": 1,
-    "BID": 0,
-    "DTg": DTg,
-    "Del": 1
-  }
-  const dataConfig = {
-    "d": d,
-    "ts": Date.now()
-  }
-  return dataConfig
-}
-// API TESTING
-
-app.get('/cfg', async (req, res) => {
-  res.send(updateTag())
-})
-
-app.get('/data', async (req, res) => {
-  let startOfDate = moment().startOf('day').format("YYYY-MM-DD HH:mm:ss")
-  let params = {
-    sNoList: "20698013,20697912,20697917,20697923,20697924,20697927,20697996,20697875,20697666,20697578,20697586,20697594",
-    sTime: startOfDate
-  }
-  const response = await axios.get('http://14.225.244.63:8083/VendingInterface.asmx/SUNGRP_getInstant', { params })
-  let xmlData = response.data
-  let jsonData = xmlParser.toJson(xmlData)
-  const valueData = JSON.parse(jsonData).DataTable['diffgr:diffgram'].DocumentElement?.dtResult
-  console.log(valueData)
-  const listModel = [20698013, 20697912, 20697917, 20697923, 20697924, 20697927, 20697996, 20697875, 20697666, 20697578, 20697586, 20697594]
-  let Val = {}
-  for (let i = 0; i < listModel.length; i++) {
-    const dataObjectFilterByModel = valueData ? valueData.filter(value => value.MA_DIEMDO === listModel[i].toString()) : []
-    const dataObject = dataObjectFilterByModel[dataObjectFilterByModel.length - 1]
-    Val[`${listModel[i]}:MA_DIEMDO`] = valueData ? parseFloat(dataObject?.MA_DIEMDO) : null
-    Val[`${listModel[i]}:SO_CTO`] = valueData ? parseFloat(dataObject?.SO_CTO) : null
-    Val[`${listModel[i]}:IMPORT_KWH`] = valueData ? parseFloat(dataObject?.IMPORT_KWH) : null
-    Val[`${listModel[i]}:EXPORT_KWH`] = valueData ? parseFloat(dataObject?.EXPORT_KWH) : null
-    Val[`${listModel[i]}:IMPORT_VAR`] = valueData ? parseFloat(dataObject?.IMPORT_VAR) : null
-    Val[`${listModel[i]}:EXPORT_VAR`] = valueData ? parseFloat(dataObject?.EXPORT_VAR) : null
-    Val[`${listModel[i]}:Ia`] = valueData ? parseFloat(dataObject?.Ia) : null
-    Val[`${listModel[i]}:Ib`] = valueData ? parseFloat(dataObject?.Ib) : null
-    Val[`${listModel[i]}:Ic`] = valueData ? parseFloat(dataObject?.Ic) : null
-    Val[`${listModel[i]}:Ua`] = valueData ? parseFloat(dataObject?.Ua) : null
-    Val[`${listModel[i]}:Ub`] = valueData ? parseFloat(dataObject?.Ub) : null
-    Val[`${listModel[i]}:Uc`] = valueData ? parseFloat(dataObject?.Uc) : null
-    Val[`${listModel[i]}:Cosphi`] = valueData ? parseFloat(dataObject?.Cosphi) : null
-    Val[`${listModel[i]}:NGAYGIO`] = valueData ? dataObject?.NGAYGIO : null
-  }
-  const data = {
-    "d": {
-      "scada_qQ2N60h1DmL": {
-        "Val": Val
-      }
-    },
-    "ts": Date.now()
-  }
-  res.send(await callAPI())
-})
-
-app.get('/data/yesterday', async (req, res) => {
-  let d = {}
-  let startOfDate = moment().startOf('day').add(-1, 'day').format("YYYY-MM-DD HH:mm:ss")
-  let params = {
-    sNoList: "20698013,20697912,20697917,20697923,20697924,20697927,20697996,20697875,20697666,20697578,20697586,20697594",
-    sTime: startOfDate
-  }
-  const response = await axios.get('http://14.225.244.63:8083/VendingInterface.asmx/SUNGRP_getInstant', { params })
-  let xmlData = response.data
-  let jsonData = xmlParser.toJson(xmlData)
-  const valueData = JSON.parse(jsonData).DataTable['diffgr:diffgram'].DocumentElement?.dtResult
-  const listModel = [20698013, 20697912, 20697917, 20697923, 20697924, 20697927, 20697996, 20697875, 20697666, 20697578, 20697586, 20697594]
-  let Val = {}
-  for (let i = 0; i < listModel.length; i++) {
-    const dataObjectFilterByModel = valueData ? valueData.filter(value => value.MA_DIEMDO === listModel[i].toString()) : []
-    const dataObject = dataObjectFilterByModel[dataObjectFilterByModel.length - 1]
-    Val[`${listModel[i]}:MA_DIEMDO`] = valueData ? parseFloat(dataObject?.MA_DIEMDO) : 0
-    Val[`${listModel[i]}:SO_CTO`] = valueData ? parseFloat(dataObject?.SO_CTO) : 0
-    Val[`${listModel[i]}:IMPORT_KWH`] = valueData ? parseFloat(dataObject?.IMPORT_KWH) : 0
-    Val[`${listModel[i]}:EXPORT_KWH`] = valueData ? parseFloat(dataObject?.EXPORT_KWH) : 0
-    Val[`${listModel[i]}:IMPORT_VAR`] = valueData ? parseFloat(dataObject?.IMPORT_VAR) : 0
-    Val[`${listModel[i]}:EXPORT_VAR`] = valueData ? parseFloat(dataObject?.EXPORT_VAR) : 0
-    Val[`${listModel[i]}:Ia`] = valueData ? parseFloat(dataObject?.Ia) : 0
-    Val[`${listModel[i]}:Ib`] = valueData ? parseFloat(dataObject?.Ib) : 0
-    Val[`${listModel[i]}:Ic`] = valueData ? parseFloat(dataObject?.Ic) : 0
-    Val[`${listModel[i]}:Ua`] = valueData ? parseFloat(dataObject?.Ua) : 0
-    Val[`${listModel[i]}:Ub`] = valueData ? parseFloat(dataObject?.Ub) : 0
-    Val[`${listModel[i]}:Uc`] = valueData ? parseFloat(dataObject?.Uc) : 0
-    Val[`${listModel[i]}:Cosphi`] = valueData ? parseFloat(dataObject?.Cosphi) : 0
-    Val[`${listModel[i]}:NGAYGIO`] = valueData ? dataObject?.NGAYGIO : " "
-  }
-  d[`${groupId}`] = {
-    "Val": Val
-  }
-  const data = {
-    "d": d,
-    "ts": Date.now()
-  }
-  client.publish(mqttTopicSendata, JSON.stringify(data))
-  res.send(data)
-})
-
-app.get('/delete/:tag', async (req, res) => {
-  try {
-    const tagDelete = req.params.tag
-    const deleteTagJson = deleteTag(tagDelete)
-    await client.publish(mqttTopicCfg, JSON.stringify(deleteTagJson), { qos: 1, retain: true })
-    res.send({ message: "Delete Sucessfully!", data: deleteTagJson })
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-app.get('/delete/device/:serial', async (req, res) => {
-  try {
-    const deviceDelete = req.params.serial
-    const deleteDeviceJson = deleteDevice(deviceDelete)
-    await client.publish(mqttTopicCfg, JSON.stringify(deleteDeviceJson), { qos: 1, retain: true })
-    res.send({ message: "Delete Sucessfully!", data: deleteDeviceJson })
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-app.get('/update', (req, res) => {
-  try {
-    const dataConfig = {
-      "d": {
-        "scada_UzVa32VanG4I": {
-          "TID": 1,
-          "Dsc": "descrp",
-          "Hbt": 60,
-          "PID": 1,
-          "BID": 0,
-          "UTg": {
-            "TEST": {
-              "Log": 1,
-              "SH": 1000,
-              "SL": 0,
-              "EU": "",
-              "DSF": "4.2",
-              "Alm": false,
-              "Name": "TEST",
-              "TID": 1,
-              "Dsc": "TEST",
-              "RO": 0,
-              "Ary": 1
-            },
-          },
-        }
-      },
-      "ts": Date.now()
-    }
-    //client.publish('iot-2/evt/waconn/fmt/scada_UzVa32VanG4I', JSON.stringify(dataConn))
-    console.log("success connect!")
-    client.publish('iot-2/evt/wacfg/fmt/scada_UzVa32VanG4I', JSON.stringify(dataConfig))
-    console.log("success config tag!")
-    res.status({ message: "Config Tag Sucessfully!", data: dataConfig })
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-//==================================
 const deviceRouter = require('./Routes/device.route')
 const tagRouter = require('./Routes/tag.route')
 const deviceTagRouter = require('./Routes/device_tag.route')
@@ -853,13 +446,6 @@ const DeleteMqttTag = async (req, res) => {
   }
 }
 
-app.get("/delete-all", (req, res) => {
-  const _messageDeleteConfigTag = DeleteTagConfigMessage.DeleteAllTag(groupId, 5)
-  console.log(_messageDeleteConfigTag)
-  const topic = mqttTopicCfg
-  client.publish(topic, JSON.stringify(_messageDeleteConfigTag), { qos: 1, retain: false });
-})
-
 app.get("/api/v1/data-hub/upload-config", async (req, res) => {
   try {
     await sendTagConfigMessage()
@@ -873,8 +459,6 @@ app.get("/api/v1/data-hub/upload-config", async (req, res) => {
     console.log(error)
   }
 })
-
-
 
 //================================================================
 // Run job every 0h5 am everyday
