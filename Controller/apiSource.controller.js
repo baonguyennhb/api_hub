1eb3bd8eca4ea6f4e6049d1cab3791c8b5aa4c90
HUU BAO <bao.nguyen@gmail.com>
2022-09-22 23:43:56 +0700

    update

--- Controller/apiSource.controller.js
+++ Controller/apiSource.controller.js
@@ -46,9 +46,9 @@ module.exports.postAdd = async (req, res) => {
         let { authorization } = data
         let sql = ''
         if (authorization) {
-            sql = `INSERT INTO ApiSource (connection_name, url, description, connection_time, interval, time_offset, is_authorization, username, password, status) Values ( '${data.connection_name}', '${data.url}' , '${data.description}' ,${data.check_connection_time}, ${data.interval}, 30, '${data.authorization}', '${data.username}', '${data.password}', 1 )`
+            sql = `INSERT INTO ApiSource (connection_name, url, key_time, description, connection_time, interval, time_offset, is_authorization, username, password, status) Values ( '${data.connection_name}', '${data.url}' , '${data.key_time}','${data.description}' ,${data.check_connection_time}, ${data.interval}, 30, '${data.authorization}', '${data.username}', '${data.password}', 1 )`
         } else {
-            sql = `INSERT INTO ApiSource (connection_name, url, description, connection_time, interval, time_offset, is_authorization, status) Values ( '${data.connection_name}', '${data.url}' , '${data.description}' ,${data.check_connection_time}, ${data.interval}, 30, '${data.authorization}', 1 )`
+            sql = `INSERT INTO ApiSource (connection_name, url,  key_time, description, connection_time, interval, time_offset, is_authorization, status) Values ( '${data.connection_name}', '${data.url}' , '${data.key_time}' , '${data.description}' ,${data.check_connection_time}, ${data.interval}, 30, '${data.authorization}', 1 )`
         }
         const devices = await query(sql)
         const dataSend = {
@@ -67,7 +67,7 @@ module.exports.postEdit = async (req, res) => {
         let id = req.query.id
         let data = req.body
         //let sql = `SELECT * FROM Metter `
-        let sql = `UPDATE ApiSource SET connection_name = '${data.connection_name}', url = '${data.url}', description = '${data.description}', interval = ${data.interval} , connection_time = ${data.check_connection_time} where id = ${id}`
+        let sql = `UPDATE ApiSource SET connection_name = '${data.connection_name}', url = '${data.url}',  key_time = '${data.key_time}', description = '${data.description}', interval = ${data.interval} , connection_time = ${data.check_connection_time} where id = ${id}`
 
         //let sql = 'SELECT * FROM Metter'
         const api = await query(sql)
